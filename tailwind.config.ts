77e1b0866c03b923a39e2f4b7624c35b1663ce79
huynamboz <teahuynam2003@gmail.com>
2024-09-13 00:11:23 +0700

    init

---
+++ tailwind.config.ts
@@ -0,0 +1,55 @@
+// const animate = require('tailwindcss-animate')
+// const { iconsPlugin, getIconCollections } = require('@egoist/tailwindcss-icons')
+
+import animate from 'tailwindcss-animate'
+
+import { iconsPlugin, getIconCollections } from '@egoist/tailwindcss-icons'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{ts,tsx,vue}',
+    './components/**/*.{ts,tsx,vue}',
+    './app/**/*.{ts,tsx,vue}',
+    './src/**/*.{ts,tsx,vue}',
+  ],
+  prefix: '',
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
+  },
+  plugins: [
+    animate,
+    iconsPlugin({
+      // Select the icon collections you want to use
+      // You can also ignore this option to automatically discover all individual icon packages you have installed
+      // If you install @iconify/json, you should explicitly specify the collections you want to use, like this:
+      collections: getIconCollections(['solar', 'material-symbols-light']),
+      // If you want to use all icons from @iconify/json, you can do this:
+      // collections: getIconCollections("all"),
+      // and the more recommended way is to use `dynamicIconsPlugin`, see below.
+    }),
+  ],
+}
